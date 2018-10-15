e04b524f2f63ea8cdd5f39602e8c7c0ebd89ebf2
Shaon Islam <sidw8@mail.umsl.edu>
2018-10-14 20:55:03 -0500

    pre-clean up

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #include <sys/ipc.h>
 #include <signal.h>
 #include <sys/wait.h>
-
+#include <time.h>
 
 
 void handle_terminate(int sig)
@@ -32,9 +32,13 @@ int main (int argc, char *argv[])
 
 
 
-	int option, max_spawn, term_time;  
+	int option, max_spawn, master_term;  
 
-        while((option = getopt(argc, argv, "hs:t:")) != -1)
+
+	srand((unsigned)time(NULL));
+	FILE *fname;   
+
+        while((option = getopt(argc, argv, "hs:t:l:")) != -1)
         {
                 switch (option)
                         {
@@ -42,22 +46,28 @@ int main (int argc, char *argv[])
                                 fprintf(stderr, "How it should run: \n");
                                 fprintf(stderr, "	-s x: x is the max number of user process to spawn (default: 5)\n");
                                 fprintf(stderr, "	-t z: z is the time in seconds when the master will terminate itself and all the children \n");
+				fprintf(stderr, "	-l y: y is the name of filename for user process information to be writting to\n");
                                 fprintf(stderr, "	-h: display options to execute\n");
                                 exit(1);
                         case 's':
 				max_spawn = atoi(optarg);
-				printf("Max user processes spawned: %d\n\n", max_spawn);
+				fprintf(stderr,"Max user processes spawned: %d\n\n", max_spawn);
 				break;
                         case 't':
-				term_time = atoi(optarg);
-				printf("Master and children will terminate in  %d seconds\n", term_time);
+				master_term = atoi(optarg);
+				fprintf(stderr,"Master and children will terminate in  %d seconds\n", master_term);
 	                       	break;
+			case 'l':
+				fname = fopen(optarg, "w+");
+				fprintf(stderr,"Logfile Name: %s\n", optarg);
+				break;
                         case '?':
                                 fprintf(stderr, "Error\n");
                                 break;
                         }
         
 	}
+
 	if (max_spawn == 0)
 	{	
 		max_spawn = 5;
@@ -66,7 +76,7 @@ int main (int argc, char *argv[])
 
 
 
-	/*_________Setup Shared Memory_________*/
+	/*_________Setup Shared Memory For Master Clock_________*/
 
 	int shm_id;
         key_t key;
@@ -91,36 +101,56 @@ int main (int argc, char *argv[])
 	master_clock[1] = nanosec;
 
 
+	/*_________Setup shmMSG_________*/
+
+        int shmMSG_id;
+        key_t key_msg;
+        void* shm_msg;
+        key_msg = 1234;
+        shmMSG_id = shmget(key_msg, sizeof(int)*2, IPC_CREAT | 0666);
+        shm_msg = shmat(shmMSG_id, NULL, 0);
+	int* term_time = shm_msg;
 
 
 
+	/*_________Creating User Processes_______*/
 
 	char arg1[10];
-	snprintf(arg1 , 10, "%d", max_spawn);
-
 	int child;
+
 	for(child = 0; child < max_spawn; child++)
 	{
-		fprintf(stderr, "\n\nTRYING to create child %d\n", (child+1));
-		
+			       
+        	int random_duration = rand() % 1000000 + 1;
+		snprintf(arg1 , 10, "%d", random_duration);
+
         	pid_t child_pid = 0;
         	child_pid = fork();
         	if (child_pid == 0)
         	{
 	        	/* in the child process! */
-        		fprintf(stderr, "i:%d 	process ID:%ld 	parent ID:%ld 	child ID:%ld\n", child, (long)getpid(), (long)getppid(), (long)child_pid);
-			execlp("./user", "./user", arg1, (char *)NULL);
+			fprintf(stderr, "\n\n Child #%d created!!!! with pid: %ld\n", (child+1), (long)getpid());
+			term_time[0] = 0;
+        		term_time[1] = 0;
+        		/*fprintf(stderr, "2 i:%d process ID:%ld 	parent ID:%ld\n", child, (long)getpid(), (long)getppid()); */
+			execlp("./user", "./user", arg1, (char *)NULL);			
 			exit(0); 
         	}
+
+		
+
+		/* READING TERMINATING TIME CLOCK FROM USER */
+		fprintf(stderr, "TERM TIME:  Seconds: %d, Nanoseconds: %d\n", term_time[0], term_time[1]);	
+
 		wait(NULL);
-
-
-		/* TESTING CHANGE IN MASTER CLOCK */
-		master_clock[0] = master_clock[0] + 100;
-                master_clock[1]	= master_clock[1] + 100;
-
 
 	}
 
+
+	/*______Clean Out Shared Memory_______*/
+	shmctl(shm_id, IPC_RMID, NULL);
+	shmctl(shmMSG_id, IPC_RMID, NULL);
+
+	fclose(fname);
 	return 0;
 }
--- user.c
+++ user.c
@@ -5,13 +5,15 @@
 #include <getopt.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
-
+#include <time.h>
 
 int main (int argc, char *argv[])
 {
 
+    
 
-	/*_________Setup Shared Memory_________*/
+
+	/*_________See Master Clock_________*/
 
 	int shm_id;
         key_t key;
@@ -19,16 +21,39 @@ int main (int argc, char *argv[])
         key = 6789;
         shm_id = shmget(key, sizeof(int)*2, IPC_CREAT | 0666);
         shm = shmat(shm_id, NULL, 0);
-
 	int* master_clock = shm;
 
-	fprintf(stderr, "Seconds: %d\n", master_clock[0]);
-        fprintf(stderr,	"Nanoseconds: %d\n", master_clock[1]);
+
+        /*_________Setup shmMSG_________*/
+	
+	int shmMSG_id;
+	key_t key_msg;
+	void* shm_msg;
+	key_msg = 1234;
+	shmMSG_id = shmget(key_msg, sizeof(int)*2, IPC_CREAT | 0666);
+        shm_msg = shmat(shmMSG_id, NULL, 0);
+
+	/*_________Put Term_time in SHM _________*/
+	
+        int* term_time = shm_msg;
+        term_time[0] = 0; /*sec*/
+        term_time[1] = 0; /*nano*/
+
+	int random_duration = atoi(argv[1]);
+	
+
+        term_time[1] = master_clock[1] + random_duration;
+	fprintf(stderr, "Master Clock + Random = Term Time: : %d + %d = %d\n", master_clock[1], random_duration, term_time[1]);
+
+        while (term_time[1] > 999999999)
+        {
+                term_time[1] = term_time[1] - 999999999;
+                term_time[0] = term_time[0] + 1;
+        }
+
+	
+	fprintf(stderr, "USER TERM TIME: %d:%d\n", term_time[0], term_time[1]);
 
 
-
-
-
-	fprintf(stderr, "in the worker \n");
 	return 0;
 }
